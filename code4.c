70f4df9206b80d5bb7b2ce4d03da2456a1f20a00
ValisoaNr <valisoandr12@gmail.com>
2025-02-10 11:54:43 +0300

    Add files via upload

---
+++ code4.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+float main ()
+{
+	float a , b , s , m ; //declaration des variables
+	
+	printf("\tSOMME ET MOYENNE\n");//presentation du programme
+	
+	printf("Entrer la valeur de la premiere note :");
+	scanf("%f",&a);
+	printf("Entrer la valeur de la deuxieme note :");
+	scanf("%f",&b);
+	
+	s = a + b ; //calcule de la somme
+	m = s / 2 ; //calcule de la moyenne
+	
+	printf("\tLeur somme vaut %f et la moyenne est de %f \n", s , m );//affichage du resultat
+	
+	
+	return (0);
+}
